1eb5e15f75444f29fd519984f03f9f32c96e0192
Ahmed Shemais <30410497+Salo7a@users.noreply.github.com>
2023-01-04 01:51:15 -0800

    init todo app.js

--- app.js
+++ app.js
@@ -11,4 +11,35 @@ const xss = require('xss-clean');
 const connectMongo = require('./db/connect');
 
 
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+
+app.use(
+    rateLimiter({
+      windowMs: 15 * 60 * 1000,
+      max: 60,
+    })
+  );
+  app.use(helmet());
+  app.use(cors());
+  app.use(xss());
+  app.use(mongoSanitize());
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+
 
