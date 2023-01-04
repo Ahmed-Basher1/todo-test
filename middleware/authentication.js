71931cae755932e993aa0658dcd7f0d5d85d78cf
Ahmed Shemais <30410497+Salo7a@users.noreply.github.com>
2023-01-04 03:39:16 -0800

    add auth and error handler and not found middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,45 @@
+/** @format */
+
+const CustomError = require('../errors');
+const { isTokenValid } = require('../utils/jwt');
+
+const authenticateUser = async (req, res, next) => {
+  let token;
+  // check header
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    token = authHeader.split(' ')[1];
+  }
+  if (!token) {
+    throw new CustomError.UnauthenticatedError(
+      'Authentication invalid'
+    );
+  }
+  try {
+    const payload = isTokenValid(token);
+    // Attach the user and his permissions to the req object
+    req.user = {
+      userId: payload.userId,
+      name: payload.name,
+      role: payload.role,
+    };
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError(
+      'Authentication invalid'
+    );
+  }
+};
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        '{"enMessage" : "Unauthorized to access this route", "arMessage" :"غير مصرح لك لإستخدام هذه الصفحة"}'
+      );
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizeRoles };
